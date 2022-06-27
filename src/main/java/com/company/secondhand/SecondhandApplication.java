424d09b0fa0c923ff56859246206bc55f66fe559
ParvinValizade <parvin.valizade@mail.ru>
2022-06-28 01:24:07 +0400

    Open API

--- src/main/java/com/company/secondhand/SecondhandApplication.java
+++ src/main/java/com/company/secondhand/SecondhandApplication.java
@@ -1,10 +1,19 @@
 package com.company.secondhand;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication(scanBasePackages = "com.company.secondhand.user")
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Secondhand-User API",
+                description = "E-commerce",
+                version = "v1"
+        )
+)
 public class SecondhandApplication {
 
     public static void main(String[] args) {
